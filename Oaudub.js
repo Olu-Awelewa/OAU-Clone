a859430fcf760a3387b978724557f5169ac8f74b
Olu-Awelewa <awelewasunkanmi@gmail.com>
2022-06-12 03:44:22 +0100

    first commit

---
+++ Oaudub.js
@@ -0,0 +1,6 @@
+let showLink = document.querySelector(".sec21")
+let btnClk = document.querySelector("#btn")
+
+btnClk.addEventListener("click", function(){
+    showLink.classList.toggle("active")
+})
